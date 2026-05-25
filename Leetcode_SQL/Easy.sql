0506306e03a7aa2c48ea18439d00bb24e8f6f3ba
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-25 21:43:25 +0530

    Update Easy.sql

--- Leetcode_SQL/Easy.sql
+++ Leetcode_SQL/Easy.sql
@@ -305,3 +305,19 @@ MAX(CASE WHEN month='Nov' THEN revenue END) AS Nov_Revenue,
 MAX(CASE WHEN month='Dec' THEN revenue END) AS Dec_Revenue
 FROM Department
 GROUP BY id;
+
+-- Day 17 | Question 24
+-- LeetCode #1211. Queries Quality and Percentage
+-- Difficulty: Easy
+-- Topic: CASE + SUM + AVG + GROUP BY
+-- Find each query_name, its quality score, and poor query percentage.
+
+SELECT
+query_name,
+ROUND(AVG(rating/position),2) AS quality,
+ROUND(
+      SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)
+      *100/COUNT(*)
+,2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
