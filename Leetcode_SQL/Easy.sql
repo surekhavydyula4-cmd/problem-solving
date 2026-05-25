625d093fba27a309bf6331b3c18a0a1787fdb08e
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-25 21:20:50 +0530

    Update Easy.sql

--- Leetcode_SQL/Easy.sql
+++ Leetcode_SQL/Easy.sql
@@ -282,3 +282,26 @@ SELECT DISTINCT author_id AS id
 FROM Views
 WHERE author_id = viewer_id
 ORDER BY id;
+
+-- Day 17 | Question 23
+-- LeetCode #1179. Reformat Department Table
+-- Difficulty: Easy
+-- Topic: CASE + MAX + GROUP BY
+-- Reformat the table such that there is a department id column and a revenue column for each month.
+
+SELECT
+id,
+MAX(CASE WHEN month='Jan' THEN revenue END) AS Jan_Revenue,
+MAX(CASE WHEN month='Feb' THEN revenue END) AS Feb_Revenue,
+MAX(CASE WHEN month='Mar' THEN revenue END) AS Mar_Revenue,
+MAX(CASE WHEN month='Apr' THEN revenue END) AS Apr_Revenue,
+MAX(CASE WHEN month='May' THEN revenue END) AS May_Revenue,
+MAX(CASE WHEN month='Jun' THEN revenue END) AS Jun_Revenue,
+MAX(CASE WHEN month='Jul' THEN revenue END) AS Jul_Revenue,
+MAX(CASE WHEN month='Aug' THEN revenue END) AS Aug_Revenue,
+MAX(CASE WHEN month='Sep' THEN revenue END) AS Sep_Revenue,
+MAX(CASE WHEN month='Oct' THEN revenue END) AS Oct_Revenue,
+MAX(CASE WHEN month='Nov' THEN revenue END) AS Nov_Revenue,
+MAX(CASE WHEN month='Dec' THEN revenue END) AS Dec_Revenue
+FROM Department
+GROUP BY id;
