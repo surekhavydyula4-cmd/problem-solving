76a4afe977e8402aa23c9ec8b252b796e20b55eb
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-30 21:41:38 +0530

    Update Easy.sql

--- Leetcode_SQL/Easy.sql
+++ Leetcode_SQL/Easy.sql
@@ -429,3 +429,17 @@ FROM Patients
 WHERE conditions LIKE 'DIAB1%'
    OR conditions LIKE '% DIAB1%';
 
+
+-- Day 21 | Question 32
+-- LeetCode #1581. Customer Who Visited but Did Not Make Any Transactions
+-- Difficulty: Easy
+-- Topic: LEFT JOIN + NULL Filtering + GROUP BY + COUNT
+-- Find customers who visited but did not make any transactions.
+
+SELECT v.customer_id,
+       COUNT(*) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+       ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
