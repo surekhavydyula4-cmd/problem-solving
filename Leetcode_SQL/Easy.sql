7c5c9b618942693bca52590a32580ca7432e51ab
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-06-02 21:55:55 +0530

    Update Easy.sql

--- Leetcode_SQL/Easy.sql
+++ Leetcode_SQL/Easy.sql
@@ -477,3 +477,20 @@ SELECT
 FROM Register
 GROUP BY contest_id
 ORDER BY percentage DESC, contest_id ASC;
+
+-- Day 24 | Question 35
+-- LeetCode #1661. Average Time of Process per Machine
+-- Difficulty: Easy
+-- Topic: SELF JOIN + GROUP BY + AVG + ROUND
+-- Find the average processing time for each machine.
+
+SELECT
+    a.machine_id,
+    ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+FROM Activity a
+JOIN Activity b
+    ON a.machine_id = b.machine_id
+    AND a.process_id = b.process_id
+WHERE a.activity_type = 'start'
+    AND b.activity_type = 'end'
+GROUP BY a.machine_id;
