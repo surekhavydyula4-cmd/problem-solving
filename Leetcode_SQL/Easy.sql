9baebcdd372b31ed969cebe5996c6c9b46ea5502
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-06-05 16:51:19 +0530

    Update Easy.sql

--- Leetcode_SQL/Easy.sql
+++ Leetcode_SQL/Easy.sql
@@ -534,3 +534,16 @@ SELECT
     COUNT(DISTINCT partner_id) AS unique_partners
 FROM DailySales
 GROUP BY date_id, make_name;
+
+-- Day 25 | Question 39
+-- LeetCode #1729. Find Followers Count
+-- Difficulty: Easy
+-- Topic: GROUP BY + COUNT + ORDER BY
+-- Count the number of followers for each user.
+
+SELECT
+    user_id,
+    COUNT(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id;
